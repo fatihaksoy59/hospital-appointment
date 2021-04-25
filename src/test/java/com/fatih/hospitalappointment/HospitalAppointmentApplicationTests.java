7e867d4d01c8ddbf786af08e81db76753c8aeca0
Fatih AKSOY <fatihaksoy59@hotmail.com>
2021-04-25 14:37:03 +0300

    first commit

---
+++ src/test/java/com/fatih/hospitalappointment/HospitalAppointmentApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fatih.hospitalappointment;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HospitalAppointmentApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
