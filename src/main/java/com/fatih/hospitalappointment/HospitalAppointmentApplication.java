66d7b95e0bf46a598149cee6bfac50e3f39c22b8
Fatih AKSOY <fatihaksoy59@hotmail.com>
2021-04-25 14:56:27 +0300

    Checkstyle added

--- src/main/java/com/fatih/hospitalappointment/HospitalAppointmentApplication.java
+++ src/main/java/com/fatih/hospitalappointment/HospitalAppointmentApplication.java
@@ -6,7 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class HospitalAppointmentApplication {
 
-	public static void main(String[] args) {
+	public static void main(final String[] args) {
 		SpringApplication.run(HospitalAppointmentApplication.class, args);
 	}
 
